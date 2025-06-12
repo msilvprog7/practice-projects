07b7212549bb46a8325019448c5a6106d977236d
Michael Snider <msilvprog7@gmail.com>
2025-06-12 12:22:57 -0700

    Thread pool practice

---
+++ concepts/multithreading/ThreadPool/Program.cs
@@ -0,0 +1,26 @@
+﻿using ThreadPool;
+
+var threadCount = 3;
+var taskCount = 15;
+var random = new Random();
+using var threadPool = new MyThreadPool(threadCount, random); // Dispose will shutdown thread pool
+var tscs = new List<Task>(taskCount); // Track the completion of tasks separate 
+
+for (var i = 0; i < taskCount; i++)
+{
+  var taskId = i;
+  var tsc = new TaskCompletionSource();
+  var task = new Func<Task>(async () =>
+  {
+    var delay = TimeSpan.FromSeconds(1 + random.Next(taskCount));
+    Console.WriteLine($"Task {taskId}, executing that takes {delay}");
+    await Task.Delay(delay);
+
+    // Complete the task
+    tsc.SetResult();
+  });
+  tscs.Add(tsc.Task);
+  threadPool.AddTask(task);
+}
+
+await Task.WhenAll(tscs);
