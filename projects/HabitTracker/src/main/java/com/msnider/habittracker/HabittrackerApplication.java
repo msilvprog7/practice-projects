ba288ba5bc8e278218a64274e820a8da8d85b594
Michael Snider <msilvprog7@gmail.com>
2025-06-24 10:58:17 -0700

    Setup SpringBoot with Maven

---
+++ projects/HabitTracker/src/main/java/com/msnider/habittracker/HabittrackerApplication.java
@@ -0,0 +1,20 @@
+package com.msnider.habittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HabittrackerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HabittrackerApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
