67dad2fba04d4b80ea25de3bfb69bb1cce9216d2
Michael Snider <msilvprog7@gmail.com>
2025-07-09 13:49:56 -0700

    Setup spring boot with gradle

---
+++ projects/ShortIdGenerator/src/main/java/com/msnider/shortidgenerator/ShortidgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.msnider.shortidgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortidgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortidgeneratorApplication.class, args);
+	}
+
+}
